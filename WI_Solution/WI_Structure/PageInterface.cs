72ccf2aef8fae7015c47242c80ba6dfbbb7e8e03
Matthan Cantrell <cantrellmatthan247@gmail.com>
2024-07-10 12:11:34 -0600

    Project Setup

---
+++ WI_Solution/WI_Structure/PageInterface.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class PageInterface
+{
+    public IPage page { get; set; }
+}
